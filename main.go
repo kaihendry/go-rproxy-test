2c9898bae0515b696d01ab7313cfdf9381583466
Kai Hendry <hendry@webconverger.com>
2017-09-26 21:32:13 +0800

    Removing World

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"regexp"
+	"strconv"
 )
+
+var changes = regexp.MustCompile(`World`)
 
 func newSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
 	rp := httputil.NewSingleHostReverseProxy(url)
@@ -14,15 +20,28 @@ func newSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
 	rp.Director = func(r *http.Request) {
 		oldDirector(r)
 		r.Host = url.Host
-		delete(r.Header, "X-Forwarded-For")
+	}
+	rp.ModifyResponse = func(resp *http.Response) (err error) {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		err = resp.Body.Close()
+		if err != nil {
+			return err
+		}
+		b = changes.ReplaceAll(b, nil)
+		body := ioutil.NopCloser(bytes.NewReader(b))
+		resp.Body = body
+		resp.ContentLength = int64(len(b))
+		resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
+		return nil
 	}
 	return rp
 }
 
 func main() {
-	serverURL, err := url.Parse("http://www.bbc.co.uk")
-	// serverURL, err := url.Parse("http://localhost:4001")
-	// serverURL, err := url.Parse("https://requestb.in/169gk011")
+	serverURL, err := url.Parse("https://hello.natalian.org")
 	if err != nil {
 		log.Fatal("URL failed to parse")
 	}
