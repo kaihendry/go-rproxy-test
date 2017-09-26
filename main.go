5c8a069f894f4a1f43b9ae02dc98405ecab416ed
Kai Hendry <hendry@webconverger.com>
2017-09-26 20:52:22 +0800

    BBC news

--- main.go
+++ main.go
@@ -5,26 +5,30 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
-func NewSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
+func newSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
 	rp := httputil.NewSingleHostReverseProxy(url)
 	oldDirector := rp.Director
 	rp.Director = func(r *http.Request) {
 		oldDirector(r)
 		r.Host = url.Host
+		delete(r.Header, "X-Forwarded-For")
 	}
 	return rp
 }
 
 func main() {
-	serverUrl, err := url.Parse("http://m.bbc.com")
+	serverURL, err := url.Parse("http://www.bbc.co.uk")
+	// serverURL, err := url.Parse("http://localhost:4001")
+	// serverURL, err := url.Parse("https://requestb.in/169gk011")
 	if err != nil {
 		log.Fatal("URL failed to parse")
 	}
-	reverseProxy := NewSingleHostReverseProxy(serverUrl)
+	reverseProxy := newSingleHostReverseProxy(serverURL)
 	http.Handle("/", reverseProxy)
-	if err = http.ListenAndServe(":9000", nil); err != nil {
+	if err = http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
 }
